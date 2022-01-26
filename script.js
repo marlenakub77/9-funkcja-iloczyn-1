1f51fc778cf13288b0a7124e3101d063d7550776
marlenakub77 <96764884+marlenakub77@users.noreply.github.com>
2022-01-26 10:02:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//  ES5
+//  function iloczyn(num1, num2, num3) {
+//    let result = num1 * num2 *num3;
+
+//    return result;
+//  }
+
+//  let wynikIlocczynu = iloczyn(2, 3, 4);
+
+//  console.log(wynikIlocczynu);
+
+
+
+
+// ES6
+  const iloczyn = (num1, num2, num3) => {
+   let result = num1 * num2 *num3;
+
+   return result;
+ }
+
+ let wynikIlocczynu = iloczyn(2,3,4);
+
+ console.log(wynikIlocczynu);
+
+
+
+//  // ES6 skrocona
+//   const iloczyn = (num1, num2, num3) => num1 * num2 *num3;
+ 
+
+//  let wynikIlocczynu = iloczyn(2,3,4);
+
+//  console.log(wynikIlocczynu);
